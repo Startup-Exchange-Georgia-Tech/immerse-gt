2b361f37bfb871a86cd19c7c6acfa65633324ba4
Yatharth Bhargava <yatharthbhargava@gmail.com>
2023-01-29 15:59:33 -0500

    Hero progress

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,12 @@ export default function Header() {
         <div className='header-link'>About</div>
         <div className='header-link'>Rules</div>
         <div className='header-link'>Schedule</div>
-        <div className='header-link'>Tracks </div>
+        <div className='header-link'>Tracks</div>
+        <div className='header-link'>FAQs</div>
+        <div className='header-link'>Contact</div>
+      </div>
+      <div className='header-apply'>
+        <div className='header-apply-button'>Apply</div>
       </div>
     </div>
   )
