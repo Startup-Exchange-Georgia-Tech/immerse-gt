21a38b3859c10cba9f56fd67383a4f1a3269e512
Yatharth Bhargava <yatharthbhargava@gmail.com>
2023-02-16 02:16:59 -0500

    initial tracks component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from "./components/Hero/Hero";
 import Hero2 from "./components/Hero2/Hero2";
 import Footer from "./components/Footer/Footer";
 import Tracks from "./components/Tracks/Tracks";
+import Tracks2 from "./components/Tracks2/tracks2";
 import Partners from "./components/Partners/Partners";
 import RegisterNowBanner from "./components/RegisterNowBanner/RegisterNowBanner";
 
@@ -11,7 +12,7 @@ function App() {
   return (
     <>
       <Hero2></Hero2>
-      <Tracks></Tracks>
+      <Tracks2></Tracks2>
       <RegisterNowBanner></RegisterNowBanner>
       <Partners></Partners>
       <Footer></Footer>
---
+++ src/components/Tracks2/tracks2.jsx
@@ -0,0 +1,21 @@
+import "./tracks2.css";
+
+export default function tracks2() {
+  return (
+    <div className="tracks2-container">
+      <div className="tracks2">
+        <div className="tracks2-text">
+          <div className="tracks2-heading">
+            CHECK OUT OUR PARTICIPATION TRACKS
+          </div>
+          <div className="tracks2-body">
+            Tracks for ImmerseGT 2023 cover all kinds of industries, ranging
+            from Web3, to Sports & Fitness. Your application will ask you to
+            list your top 3 tracks.For more information on how each track is
+            evaluated, view our Participation Tracks Notion page here.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
