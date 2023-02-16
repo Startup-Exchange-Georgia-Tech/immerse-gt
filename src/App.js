a84ba5aae280daaba3362124c8c2199fbb49ae2d
Yatharth Bhargava <yatharthbhargava@gmail.com>
2023-02-15 23:51:57 -0500

    partners section

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Hero from "./components/Hero/Hero";
 import Hero2 from "./components/Hero2/Hero2";
 import Footer from "./components/Footer/Footer";
 import Tracks from "./components/Tracks/Tracks";
+import Partners from "./components/Partners/Partners";
 
 function App() {
   return (
     <>
       <Hero2></Hero2>
       <Tracks></Tracks>
+      <Partners></Partners>
       <Footer></Footer>
     </>
   );
