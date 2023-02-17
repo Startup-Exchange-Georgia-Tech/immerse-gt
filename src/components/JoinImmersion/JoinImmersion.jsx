8ba95d5163eb5b5af94081a118cfd70d2529f45b
Yatharth Bhargava <yatharthbhargava@gmail.com>
2023-02-17 01:52:41 -0500

    speakers section

--- src/components/JoinImmersion/JoinImmersion.jsx
+++ src/components/JoinImmersion/JoinImmersion.jsx
@@ -24,14 +24,12 @@ export default function JoinImmersion() {
 
   const getReturnValues = (countDown) => {
     // calculate time left
-    console.log(countDown);
     const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
     const hours = Math.floor(
       (countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
     );
     const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
-    console.log([days, hours, minutes, seconds]);
     return [days, hours, minutes, seconds];
   };
   return (
--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -14,7 +14,7 @@ export default function Partners() {
         <div className="partners-section-button">
           <a href="mailto:hello@immersegt.io">
             <div className="hero-partner">
-              Partner with us{" "}➫
+              Partner with us ➫
               {/* <span className="arrow">
                 <img src="/arrow.png"></img>
               </span> */}
@@ -31,7 +31,7 @@ export default function Partners() {
           {/* <img src="/createx.png" className="partner-image"></img> */}
         </div>
         <div className="partner-image-container">
-         {/* <img src="/404dao.png" className="partner-image"></img> */}
+          {/* <img src="/404dao.png" className="partner-image"></img> */}
         </div>
         {/* <div className="partner-image-container">
           <img src="/google.png" className="partner-image"></img>
@@ -46,8 +46,57 @@ export default function Partners() {
           <img src="/accenture.png" className="partner-image"></img>
         </div> */}
       </div>
-      <div className="partners-bottom">
-
+      <div className="partners-bottom"></div>
+      <div className="keynote-speakers-section">
+        <div className="partners-heading keynote-speakers-heading">
+          KEYNOTE SPEAKERS
+        </div>
+        <div className="keynote-speakers-container">
+          <div className="keynote-speaker-card">
+            <div className="keynote-speaker-card-header">
+              <div className="keynote-speaker-card-title">DAN HELFRICH</div>
+              <div className="keynote-speaker-card-role">
+                CEO & CHAIRMAN, DELOITTE CONSULTING
+              </div>
+            </div>
+            <div className="keynote-speaker-card-bottom">
+              <img
+                src="./dan.png"
+                alt="Image of Dan HelFrich"
+                className="keynote-image"
+              />
+              <div className="keynote-speaker-card-text">
+                Dan Helfrich is chair and chief executive officer of Deloitte
+                Consulting LLP, where he leads a growing team of 80,000+
+                professionals who help clients innovatively face today's most
+                complex issues.
+              </div>
+            </div>
+          </div>
+          <div className="keynote-speaker-card">
+            <div className="keynote-speaker-card-header">
+              <div className="keynote-speaker-card-title">AYO ODUSOTE</div>
+              <div className="keynote-speaker-card-role">
+                PRINCIPAL & DEI LEADER, DELOITTE
+              </div>
+            </div>
+            <div className="keynote-speaker-card-bottom">
+              <img
+                src="./ayo.png"
+                alt="Image of Dan HelFrich"
+                className="keynote-image"
+              />
+              <div className="keynote-speaker-card-text">
+                Ayo is a principal in Deloitte Consulting LLP’s customer &
+                marketing practice with 20 years of consulting experience,
+                serving clients in the Technology, Media, and Telecom sectors.
+                Ayo also serves as the Diversity, Equity, and Inclusion leader
+                for Deloitte Consulting.<br></br>Ayo also serves as the
+                Diversity, Equity, and Inclusion leader for Deloitte Consulting.
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
